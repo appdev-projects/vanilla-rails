f421340b40d06ed79421cb3b6e8d26c206e00fe4
Dayton-Killian-1 <dayton.killian@chicagobooth.edu>
2022-06-01 22:19:52 +0000

    Cards installed on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   
   resources :comments
   resources :votes
-  resources :moves
+  resources :moves#, only: [:index, :create, :delete, :update] <- need to update all of the routes to get rid of the routes we don't need. Also, confirm the action names (i think it's delete, but might be remove or destroy or something)
   resources :games
   resources :pieces
   devise_for :users
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
