c41ea53279f1316dd9e49467e88090746ba6dcf9
Dayton-Killian-1 <dayton.killian@chicagobooth.edu>
2022-06-01 23:09:55 +0000

    trying to rebase

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ApplicationRecord
   has_many  :moves, dependent: :destroy
   has_many  :comments, dependent: :destroy
+
+  enum status: { active: "active", complete: "complete" }
+
+  scope :active, -> { where(status: "active") }
 end
