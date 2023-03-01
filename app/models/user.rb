485cabcfb67d0ce74f05c6266cbc08dd44799a20
dmacinto <dmacinto@gmail.com>
2023-03-01 16:38:41 +0000

    added devise and updated user model

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  acts_as_token_authenticatable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many  :tasks, class_name: "Task", foreign_key: "commenter_id", dependent: :destroy
+
+  validates :username,
+    presence: true,
+    uniqueness: true,
+    format: { 
+      with: /\A[\w_\.]+\z/i,
+      message: "can only contain letters, numbers, periods, and underscores"
+    }
+
+
+  def ensure_website_has_scheme
+    if website.present? &&
+      !website.starts_with?("http://") &&
+      !website.starts_with?("https://")
+
+      self.website = "http://" + self.website
+    end
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.hosts.clear
   # Allow all Gitpod IPs to render the BetterErrors page
   config.web_console.whitelisted_ips = '0.0.0.0/0.0.0.0'
