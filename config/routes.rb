4241035822b88accbdecf6dca49b48ca1f15d0b1
disanchez27edu <disanchez@uchicago.edu>
2023-06-12 21:36:56 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
-  resources :assessment_scores
+
+  root "application#welcome"
+
   resources :lesson_events
-  resources :assessment_questions
-  resources :lessons
-  resources :courses
-  devise_for :seekers
+  resources :assessment_scores
+  devise_for :seekers, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :courses do
+    resources :lessons
+  end
+
+  devise_scope :seeker do
+  get "/welcome/index", to: "registrations#welcome"
+  end
+
+  #data to csv export routes
+  get("/export_courses", { :controller => "courses", :action => "export" })
+  get("/export_lessons", { :controller => "lessons", :action => "export" })
+  get("/export_aq", { :controller => "assessment_questions", :action => "export" })
+  get("/export_as", { :controller => "assessment_scores", :action => "export" })
+
 end
